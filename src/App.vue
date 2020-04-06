565e814934606bd5175cc2ea2cebad0ccf1c6121
Woody <woody.chang@cloud-interactive.com>
2020-04-06 16:05:51 +0800

    completed question app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,62 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Header :numCorrect="numCorrect" :numTotal="numTotal" />
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="12" offset="0">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
+
+<script>
+import Header from './components/Header'
+import QuestionBox from './components/QuestionBox'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    }
+  },
+  methods: {
+    next() {
+      this.index++
+    },
+    increment(isCorrect) {
+      if (isCorrect) this.numCorrect++
+      this.numTotal++
+    }
+  },
+  mounted: function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+      method: 'get'
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(jsonData => {
+        // console.log(jsonData)
+        this.questions = jsonData.results
+      })
+  }
+}
+</script>
 
 <style>
 #app {
